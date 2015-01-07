975e278734e449eca43115034c8e001d4896e542
Chew Lam <chew@tactile.com>
2015-01-06 16:36:31 -0800

    fixes to start_web.sh

--- docker-web/include/start_web.sh
+++ docker-web/include/start_web.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
 
 cd /root/src
-/var/lib/gems/1.9.1/gems/rack-1.5.2/bin/rackup -p 4567 -o 0.0.0.0 config.ru
-
+/var/lib/gems/1.9.1/gems/rack-*/bin/rackup -p 4567 -o 0.0.0.0 -O DoNotReverseLookup config/config.ru
